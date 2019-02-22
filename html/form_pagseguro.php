df2707a138087075266951e2ad1c62e09af70880
mateus3621 <mmaserver@hotmail.com>
2019-02-22 19:49:36 -0300

    Site
    site

---
+++ html/form_pagseguro.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once 'custom_scripts/PagSeguroLibrary/PagSeguroLibrary.php';
+require 'config/config.php';
+
+$paymentRequest = new PagSeguroPaymentRequest();
+$paymentRequest->addItem('1', $config['pagSeguro']['productName'], $_POST['itemCount'], $config['pagSeguro']['productValue']);
+$paymentRequest->setCurrency("BRL");
+$paymentRequest->setReference($_POST['reference']);
+$paymentRequest->setRedirectUrl($config['pagSeguro']['urlRedirect']);
+$paymentRequest->addParameter('notificationURL', $config['pagSeguro']['urlNotification']);
+
+try {
+
+	$credentials = PagSeguroConfig::getAccountCredentials();
+	$checkoutUrl = $paymentRequest->register($credentials);
+	header('location:' . $checkoutUrl);
+
+} catch (PagSeguroServiceException $e) {
+	die($e->getMessage());
+}
+
+?>
